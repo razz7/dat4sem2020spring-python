4c206d07092f0eb34af3d9b2476c22bf907f0f8c
Rasmus Hemmingsen <rashem2@hotmail.com>
2020-02-24 09:23:13 +0100

    a

---
+++ week3/EX1/student.py
@@ -0,0 +1,10 @@
+class Student():
+    def __init__(self, name, data_sheet):
+        self.name = name
+        slef.data_sheet = data_sheet
+
+
+    def __repr__(self):
+        return 'Student(%r, %r) % (self.name, self.data_sheet)
+
+
